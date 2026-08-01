a11d2f30845c825ab2545aba9238f807eaf99b97
AutumnVN <autumnvnchino@gmail.com>
2026-08-01 17:13:24 +0700

    gf kari speedup animation

---
+++ vcard.ameba.jp/Girl-Friend-Kari-speedup-animation.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name        Girl Friend Kari speedup animation
+// @description Speed up animation to reduce waiting time
+// @run-at      document-start
+// @grant       none
+// @version     1.0
+// @author      AutumnVN
+// @match       https://vcard.ameba.jp/*
+// @homepageURL https://github.com/AutumnVN/userscript
+// @downloadURL https://github.com/AutumnVN/userscript/raw/main/vcard.ameba.jp/Girl-Friend-Kari-speedup-animation.js
+// @updateURL   https://github.com/AutumnVN/userscript/raw/main/vcard.ameba.jp/Girl-Friend-Kari-speedup-animation.js
+// ==/UserScript==
+
+const SPEED_MULTIPLIER = 16;
+const originalSetTimeout = window.setTimeout;
+const originalSetInterval = window.setInterval;
+const originalRequestAnimationFrame = window.requestAnimationFrame;
+
+window.setTimeout = function (callback, delay, ...args) {
+    if (document.querySelector('canvas')) {
+        delay = delay / SPEED_MULTIPLIER;
+    }
+    return originalSetTimeout(callback, delay, ...args);
+}
+
+window.setInterval = function (callback, delay, ...args) {
+    if (document.querySelector('canvas')) {
+        delay = delay / SPEED_MULTIPLIER;
+    }
+    return originalSetInterval(callback, delay, ...args);
+}
+
+window.requestAnimationFrame = function (callback) {
+    return originalRequestAnimationFrame(function (timestamp) {
+        if (document.querySelector('canvas')) {
+            timestamp = timestamp * SPEED_MULTIPLIER;
+        }
+        callback(timestamp);
+    });
+};
